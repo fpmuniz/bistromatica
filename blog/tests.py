407b4022d2fbf79ceaee5b617f602973d7767872
Felipe Muniz <muniz.r.felipe@gmail.com>
2020-07-05 10:52:35 -0300

    Added factories and simple tests for Post

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,28 @@
+import factory
 from django.test import TestCase
 
+from django.contrib.auth.models import User
+from .models import Post
+
 # Create your tests here.
+class UserFactory(factory.DjangoModelFactory):
+	class Meta:
+		model = User
+
+	username = factory.Faker('user_name')
+	password = factory.Faker('password')
+
+
+class PostFactory(factory.DjangoModelFactory):
+	class Meta:
+		model = Post
+
+	creator = factory.LazyFunction(UserFactory.create)
+	title = factory.Faker('sentence')
+	content = factory.Faker('paragraphs')
+
+class PostTestCase(TestCase):
+	def test_publish(self):
+		post = PostFactory.create()
+		post.publish()
+		self.assertIsNotNone(post.published_at)
