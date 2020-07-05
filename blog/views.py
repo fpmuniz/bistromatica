d75e98a17b4591efe3f70d68381bd358f933e02b
Felipe Muniz <muniz.r.felipe@gmail.com>
2020-07-05 18:25:46 -0300

    Added list views for posts
    - Created blog.urls
    - Including blog.urls in root urls
    - Created PostListView
    - Created index.html template with list of posts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Post
 
 # Create your views here.
+class PostListView(ListView):
+	model = Post
+	paginate_by = 10
+	template_name = 'index.html'
