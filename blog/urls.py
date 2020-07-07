7d1e7d90be9d2f214af5f48265f2ab143d18b2b0
Felipe Muniz <muniz.r.felipe@gmail.com>
2020-07-07 06:58:21 -0300

    Require login for post edition

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('posts/', views.PostListView.as_view(), name='post_list'),
     path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('posts/<int:pk>/edit', views.PostEditView.as_view(), name='post_edit'),
+    path('posts/edit', views.PostEditView.as_view(), name='post_edit'),
     path('', views.index, name='index')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def index(request):
@@ -22,6 +26,17 @@ class PostDetailView(DetailView):
 	template_name = 'blog/post_detail.html'
 
 
-class PostEditView(DetailView):
+class PostEditView(LoginRequiredMixin, DetailView):
 	model = Post
 	template_name = 'blog/post_edit.html'
+
+	def get(self, request, pk=None, *args, **kwargs):
+		template = loader.get_template('blog/post_edit.html')
+		if pk is None:
+			user = User.objects.get(pk=request.user.pk)
+			post = Post(creator=user)
+		else:
+			post = Post.all_objects.get(pk=pk)
+		form = PostForm(instance=post)
+		context = {'form': form}
+		return HttpResponse(template.render(context, request))
