c4839f869c04ef8b6974aa02ea309528829fc09c
Felipe Muniz <muniz.r.felipe@gmail.com>
2020-07-11 20:21:14 -0300

    Add ModelForm for Thread model

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 
-from .models import Post
+from .models import Post, Thread
 
 
 class PostForm(forms.ModelForm):
 	class Meta:
 		model = Post
-		fields = ('title', 'content')
+		fields = ('title', 'content', 'thread')
+
+
+class ThreadForm(forms.ModelForm):
+	class Meta:
+		model = Thread
+		fields = ('name', 'description')
