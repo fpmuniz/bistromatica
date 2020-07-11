27ae677c041fc0da4f2a12d605904bd163e6f6a4
Felipe Muniz <muniz.r.felipe@gmail.com>
2020-07-11 20:51:13 -0300

    Add simple tests for Threads

--- blog/tests.py
+++ blog/tests.py
@@ -1,8 +1,10 @@
+from datetime import datetime
+
 import factory
 from django.test import TestCase
-
 from django.contrib.auth.models import User
-from .models import Post
+
+from .models import Post, Thread
 
 # Create your tests here.
 class UserFactory(factory.DjangoModelFactory):
@@ -13,13 +15,27 @@ class UserFactory(factory.DjangoModelFactory):
 	password = factory.Faker('password')
 
 
+class ThreadFactory(factory.DjangoModelFactory):
+	class Meta:
+		model = Thread
+
+	name = factory.Faker('sentence')
+	description = factory.Faker('text')
+
+
 class PostFactory(factory.DjangoModelFactory):
 	class Meta:
 		model = Post
 
 	creator = factory.LazyFunction(UserFactory.create)
+	thread = factory.LazyFunction(ThreadFactory.create)
 	title = factory.Faker('sentence')
 	content = factory.Faker('text')
+
+
+class PublishedPostFactory(PostFactory):
+	published_at = factory.LazyFunction(datetime.now)
+
 
 class PostTestCase(TestCase):
 	def setUp(self):
@@ -37,3 +53,11 @@ class PostTestCase(TestCase):
 		self.post.publish()
 		with self.assertWarns(self.post.AlreadyPublishedWarning):
 			self.post.publish()
+
+class ThreadTestCase(TestCase):
+	def setUp(self):
+		self.thread = ThreadFactory.create()
+
+	def test_simple_thread(self):
+		PublishedPostFactory.create_batch(3, thread=self.thread)
+		self.assertEqual(self.thread.post_set.count(), 3)
