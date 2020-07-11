908c71f661d416d067f67a99435e0f29595fab89
Felipe Muniz <muniz.r.felipe@gmail.com>
2020-07-11 20:20:55 -0300

    Include Thread model in admin interface

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from blog.models import Post
+from blog.models import Post, Thread
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
 	pass
 
+
+class ThreadAdmin(admin.ModelAdmin):
+	pass
+
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(Thread, ThreadAdmin)
