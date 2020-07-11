e8908c89e3ce77ffbb147400946bebdab8c95c42
Felipe Muniz <muniz.r.felipe@gmail.com>
2020-07-11 20:18:22 -0300

    Forcing SSL on non-DEBUG environments to postgres

--- bistromatica/settings.py
+++ bistromatica/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
     # }
 }
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=False)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=not DEBUG)
 
 
 # Password validation
