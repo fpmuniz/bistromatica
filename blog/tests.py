d724e71e8de649deb90e8c3b599c6c2ff3b788cf
Felipe Muniz <muniz.r.felipe@gmail.com>
2020-07-12 16:28:47 -0300

    Add testing for Post views

--- blog/tests.py
+++ blog/tests.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import factory
 from django.test import TestCase
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 from .models import Post, Thread
 
@@ -51,6 +52,8 @@ class PostTestCase(TestCase):
 		self.assertEqual(Post.objects.count(), 0)
 		self.assertEqual(Post.all_objects.count(), 1)
 
+
+
 class ThreadTestCase(TestCase):
 	def setUp(self):
 		self.thread = ThreadFactory.create()
@@ -58,3 +61,46 @@ class ThreadTestCase(TestCase):
 	def test_simple_thread(self):
 		PublishedPostFactory.create_batch(3, thread=self.thread)
 		self.assertEqual(self.thread.post_set.count(), 3)
+
+class TemplateViewTestCase(TestCase):
+	def test_index(self):
+		response = self.client.get(reverse('index'))
+		self.assertLess(response.status_code, 400)
+
+	def test_about(self):
+		response = self.client.get(reverse('about'))
+		self.assertEqual(response.status_code, 200)
+
+	def test_donate(self):
+		response = self.client.get(reverse('donate'))
+		self.assertEqual(response.status_code, 200)
+
+
+class PostViewTestCase(TestCase):
+	def setUp(self):
+		self.user = UserFactory.create()
+
+	def test_post_list_redirect_published_only_when_not_logged_in(self):
+		response = self.client.get(reverse('post_list'))
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response.url, reverse('post_public_list'))
+
+	def test_post_list_redirects_to_all_posts_when_logged_in(self):
+		self.client.force_login(self.user)
+		response = self.client.get(reverse('post_list'))
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response.url, reverse('post_full_list'))
+
+	def test_redirect_post_creation_to_anon_user(self):
+		response = self.client.get(reverse('post_create'))
+		self.assertEqual(response.status_code, 302)
+
+	def test_get_create_post(self):
+		self.client.force_login(self.user)
+		response = self.client.get(reverse('post_create'))
+		self.assertEqual(response.status_code, 200)
+
+	def test_create_post(self):
+		self.client.force_login(self.user)
+		response = self.client.post(reverse('post_create'))
+		self.assertEqual(response.status_code, 200)
